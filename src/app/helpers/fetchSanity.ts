8130598d832acdf21efe770e2b66e688b6f2a873
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-12-11 13:58:53 +0100

    refactor: use async await

--- src/app/helpers/fetchSanity.ts
+++ src/app/helpers/fetchSanity.ts
@@ -1,32 +1,40 @@
 const ENDPOINT = 'https://dhh9ms17.api.sanity.io/v1/graphql/production/default';
 
-function fetchSanity<ReturnType>(
+async function fetchSanity<ReturnType>(
   query: string,
   variables: Record<any, string> | undefined = undefined
 ) {
   const CACHE_TIME = 3600 * 24; // 24h
 
-  return fetch(ENDPOINT, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Authorization: `Bearer ${process.env.SANITY_TOKEN}`,
-    },
-    body: JSON.stringify({
-      query,
-      variables,
-    }),
-    next: { revalidate: CACHE_TIME },
-  })
-    .then((r) => r.json())
-    .then((result) => {
-      return result.data as ReturnType;
-    })
-    .catch((error: unknown) => {
-      console.error('👨‍🚒', ENDPOINT);
-      console.error('👨‍🚒', error);
+  try {
+    const response = await fetch(ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${process.env.SANITY_TOKEN}`,
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+      next: { revalidate: CACHE_TIME },
     });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    if (response.status === 200) {
+      const responseJson = await response.json();
+      return responseJson.data as ReturnType;
+    } else {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error('👨‍🚒', ENDPOINT);
+    console.error('👨‍🚒', error);
+    throw error;
+  }
 }
 
 export default fetchSanity;
